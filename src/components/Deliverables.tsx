607c30b08860b89732013fc94d84d3a8e0cae2a4
Mohad Naeem <mohad238695@gmail.com>
2025-04-30 13:13:33 +0500

    Reponsiveness Fix

--- src/components/Deliverables.tsx
+++ src/components/Deliverables.tsx
@@ -11,48 +11,53 @@ export default function Deliverables() {
           Deliverables Requested
         </h2>
 
-        {/* Content Rows */}
-        <div className="pl-45 space-y-10">
+        <div className="space-y-10">
           {/* Instagram */}
-          <div className="flex items-start gap-6">
-            <Image
-              src="/insta-logo.png"
-              alt="Instagram Logo"
-              width={150}
-              height={40}
-            />
-            <p className="text-black text-base leading-relaxed max-w-xl">
-              Lorem ipsum dolor sit amet, consectetur adipiscing <br /> elit.
-              Donec quis nisl feugiat.
-            </p>
+          <div className="flex justify-center">
+            <div className="flex items-start gap-6">
+              <Image
+                src="/insta-logo.png"
+                alt="Instagram Logo"
+                width={150}
+                height={40}
+              />
+              <p className="text-black text-base leading-relaxed max-w-xl">
+                Lorem ipsum dolor sit amet, consectetur adipiscing <br /> elit.
+                Donec quis nisl feugiat.
+              </p>
+            </div>
           </div>
 
           {/* TikTok */}
-          <div className="flex items-start gap-6">
-            <Image
-              src="/tiktok-logo.png"
-              alt="TikTok Logo"
-              width={150}
-              height={40}
-            />
-            <p className="text-black text-base leading-relaxed max-w-xl">
-              Lorem ipsum dolor sit amet, consectetur adipiscing <br /> elit.
-              Donec quis nisl feugiat.
-            </p>
+          <div className="flex justify-center">
+            <div className="flex items-start gap-6">
+              <Image
+                src="/tiktok-logo.png"
+                alt="TikTok Logo"
+                width={150}
+                height={40}
+              />
+              <p className="text-black text-base leading-relaxed max-w-xl">
+                Lorem ipsum dolor sit amet, consectetur adipiscing <br /> elit.
+                Donec quis nisl feugiat.
+              </p>
+            </div>
           </div>
 
           {/* YouTube */}
-          <div className="flex items-start gap-6">
-            <Image
-              src="/yt-logo.png"
-              alt="YouTube Logo"
-              width={150}
-              height={40}
-            />
-            <p className="text-black text-base leading-relaxed max-w-xl">
-              Lorem ipsum dolor sit amet, consectetur adipiscing <br /> elit.{" "}
-              Donec quis nisl feugiat.
-            </p>
+          <div className="flex justify-center">
+            <div className="flex items-start gap-6">
+              <Image
+                src="/yt-logo.png"
+                alt="YouTube Logo"
+                width={150}
+                height={40}
+              />
+              <p className="text-black text-base leading-relaxed max-w-xl">
+                Lorem ipsum dolor sit amet, consectetur adipiscing <br /> elit.
+                Donec quis nisl feugiat.
+              </p>
+            </div>
           </div>
         </div>
       </div>
